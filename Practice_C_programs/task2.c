f1ab5dea62e007a5731e05c6596b0dfd57409315
SIRREG001 <udochukwureginald78@gmail.com>
2022-03-22 01:27:54 -0700

    Favorate Song -Lose yourself by Eminem

---
+++ Practice_C_programs/task2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * This program prints the verse 3 of "lose yourself" song -Eminem
+ * This is my favorite song and i am a huge fan of Eminem
+ */
+
+int main(void)
+{
+	printf("No more games, I'ma change what you call rage\n");
+	printf("Tear this motherfuckin' roof off like two dogs caged\n");
+	printf("I was playing in the beginning, the mood all changed\n");
+	printf("I've been chewed up and spit out and booed off stage\n");
+	printf("But I kept rhymin' and stepped right in the next cypher\n");
+	printf("Best believe somebody's payin' the Pied Piper\n");
+	printf("All the pain inside amplified by the\n");
+	printf("Fact that I can't get by with my nine-to-\n");
+	printf("Five and I can't provide the right type of life for my family\n");
+	printf("'Cause man, these goddamn food stamps don't buy diapers\n");
+	printf("And is no movie, there's no Mekhi Phiber, this is my life\n");
+	printf("And these times are so hard, and it's gettin' even harder\n");
+	printf("Tryna feed and water my seed, plus teeter-totter\n");
+	printf("Caught up between bein' a father and a prima donna\n");
+	printf("Baby mama drama, screamin' on her, too much for me to wanna\n");
+	printf("Stay in one spot, another day of monotony's\n");
+	printf("Gotten me to the point I'm like a snail, I've got\n");
+	printf("To formulate a plot or end up in jail or shot\n");
+	printf("Success is my only motherfuckin' option, failure's not\n");
+	printf("Mom, I love you, but this trailer got\n");
+	printf("To go; I cannot grow old in Salem's Lot\n");
+	printf("So here I go, it's my shot, feet, fail me not\n");
+	printf("This may be the only opportunity that I got\n");
+}
