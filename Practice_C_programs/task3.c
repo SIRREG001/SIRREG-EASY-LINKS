ff0b7799be151ead7fe54f044127166c39295bf2
SIRREG001 <udochukwureginald78@gmail.com>
2022-03-22 01:41:49 -0700

    Addition

---
+++ Practice_C_programs/task3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * This program prints the sum of 29 and 5
+ * To declare variables in C and assign values to them
+ */
+
+int main(void)
+{
+	int a, b, sum;
+	a = 29;
+	b = 5;
+	sum = a + b;
+	printf("%d + %d = %d\n", a, b, sum);
+}
