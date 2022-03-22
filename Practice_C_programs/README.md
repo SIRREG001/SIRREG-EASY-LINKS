891b65e61efab734b38ec75008566adddcfe5a1d
SIRREG001 <udochukwureginald78@gmail.com>
2022-03-22 00:50:35 -0700

    Tide in the affairs of men

---
+++ Practice_C_programs/README.md
@@ -0,0 +1,1 @@
+task1.c prints There is a tide in the affairs of men Which, taken at the flood, leads on to fortune.
---
+++ Practice_C_programs/task1.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+/**
+ *  main - main block
+ *  This code prints There is a tide in the affairs of men
+ *  Which, taken at the flood, leads on to fortune
+ */
+int main(void)
+{
+	printf("There is a tide in the affairs of men\n");
+	printf("Which taken at the flood, leads on to fortune\n");
+
+	return (0);
+}
