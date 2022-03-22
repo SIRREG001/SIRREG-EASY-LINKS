dfa4e2c17244f57631ee84eda76d9fe275b84344
SIRREG001 <udochukwureginald78@gmail.com>
2022-03-22 15:02:24 -0700

    Product of two numbers

---
+++ Practice_C_programs/task4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * This program finds the product of 29 and 5
+ * A basic arithmetic operation
+ */
+int main(void)
+{
+	int a, b, product;
+	
+	a = 29;
+	b = 5;
+	product = a * b;
+	printf("The product of %d and %d is %d\n", a, b, a * b);
+}
